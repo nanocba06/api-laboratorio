5fffa31a69f1424c93deb6507b294fedcb81e43c
Hernan <hernanherrador@gmail.com>
2022-07-06 22:03:50 -0300

    Api Laboratorios

---
+++ app/Http/Controllers/Api/LaboratorioController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Laboratorio;
+use Illuminate\Http\Request;
+
+class LaboratorioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $laboratorios = Laboratorio::all();
+        return $laboratorios;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $laboratorio = new Laboratorio();
+        $laboratorio->nombre = $request->nombre;
+
+        $laboratorio->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $laboratorio = Laboratorio::find($id);
+        return $laboratorio;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $laboratorio = Laboratorio::findOrFail($request->id);
+        $laboratorio->nombre = $request->nombre;
+
+        $laboratorio->save();
+        return $laboratorio;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $laboratorio = Laboratorio::destroy($id);
+        return $laboratorio;
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\Api\LaboratorioController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::controller(LaboratorioController::class)->group(function(){
+    Route::get('/laboratorios','index');
+    Route::post('/laboratorio','store');
+    Route::get('/laboratorio/{id}','show');
+    Route::put('/laboratorio/{id}','update');
+    Route::delete('/laboratorio/{id}','destroy');
+});
